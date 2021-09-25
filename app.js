d1634cbbf4e1fa73389fb648094822c57c33f22f
edgelock <44417754+edgelock@users.noreply.github.com>
2021-09-24 23:06:51 -0400

    that didn't work

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
+const express = require("express");
+const app = express();
 
-var express = require("express");
-var path = require("path");
+app.get("/", function (req, res) {
+  res.send("IT WORKED");
+});
+app.get("/new", function (req, res) {
+  res.send("New also worked");
+});
 
-var routes = require("./routes");
-
-var app = express();
-
-app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(routes);
-
-app.listen(app.get("port"), function(){
-    console.log("Server started on port " + app.get("port"));
-})
+app.listen(process.env.PORT || 5000);
+module.exports = app;
